fdf005d5b3cb5b0462d087b940d786d4ccb76449
journeytozero <falcon.user.trk@gmail.com>
2026-08-31 22:01:55 +0600

    postgre sql connected

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.ainvamrqpmkxfjhsvuch:ExpeseTracker@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgresr"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.ainvamrqpmkxfjhsvuch:ExpeseTracker@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,)
 
